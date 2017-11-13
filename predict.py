401e88546ef81bb1c825ab32bf74d3b7f1cc7420
Jeff Macaluso <JeffMacaluso@users.noreply.github.com>
2017-11-12 22:18:55 -0600

    Fixed to match swagger definition

--- predict.py
+++ predict.py
@@ -5,18 +5,23 @@ Submits a request to the web service and returns a prediction
 import json
 import requests
 import numpy as np
-from azureml.api.schema.sampleDefinition import SampleDefinition
-from azureml.api.schema.dataTypes import DataTypes
 
 # Generating sample data
-sample_input = (np.random.rand(28, 28, 1) * 255)
+sample_input = (np.random.rand(28, 28, 1))
 sample_input = sample_input.reshape(1, 28, 28, 1)  # Reshaping to match training data
 
-# Shaping into the format: "{\"input_df\": [{\"feature1\": value1, \"feature2\": value2}]}"
+# Shaping into the format: "{\"input_df\": [{\"feature1\": value1, \"feature2\": value2}]}"D
 datalist = sample_input.tolist()  # Converting to a list before converting to JSON
-data = json.dumps(datalist, separators=(',', ':'), sort_keys=True, indent=4)  # JSONifying the multi-dimensional array
-body = str.encode(data)  # Encoding as a string
+data = {"input_array": datalist}  # Adding the "input_array" header
+data = json.dumps(data, separators=(',', ':'), sort_keys=True, indent=4)  # JSONifying the multi-dimensional array
+data = str.encode(data)  # Encoding as a string before passing to the RESTful API
 
+### Calling from the local service
+# Setting up the request parameters
+# url = 'http://40.84.6.42:80/api/v1/service/mnistcluster/score'
+# headers = {'Content-Type':'application/json'}
+
+### Calling from the kubernetes cluster service
 # Setting up the request parameters
 url = 'http://40.84.6.42:80/api/v1/service/mnistcluster/score'
 api_key = 'b91e271f06794fdea8085ab2dc032adb'  # Warning: It's bad practice to share API keys to the public
